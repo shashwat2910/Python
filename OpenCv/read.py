9761cc7a3198efa45536f5afd4813fc8d9fb6afc
Shashwat Sharma <shashwat2910@gmail.com>
2021-02-02 10:49:08 +0530

    video cam on

---
+++ OpenCv/read.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+# To open the web cam of your camera
+
+# capture = cv.VideoCapture(0)
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow('Video', frame)
+
+#     if(cv.waitKey(20) & 0xFF==ord('d')):
+#         break
+# capture.release()
+# cv.destroyAllWindows()
