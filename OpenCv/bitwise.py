4b8032a4ee751550e8e32c3b3ef78ed960ff13d1
Shashwat Sharma <shashwat2910@gmail.com>
2021-02-04 15:50:45 +0530

    bitwise done

---
+++ OpenCv/bitwise.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((400,400), dtype='uint8')
+
+rectangle = cv.rectangle(blank.copy(), (30,30),(370,370),255,-1)
+circle = cv.circle(blank.copy(), (200,200), 200, 255,-1)
+
+cv.imshow('Rectangle', rectangle)
+cv.imshow('Circle', circle)
+
+# bitwise And
+
+bitwise_and = cv.bitwise_and(rectangle, circle)
+cv.imshow('Bitwise And', bitwise_and)
+
+# bitwise OR
+
+bitwise_or = cv.bitwise_or(rectangle, circle)
+cv.imshow('Bitwise Or', bitwise_or)
+
+# bitwise XOR
+
+bitwise_xor = cv.bitwise_xor(rectangle, circle)
+cv.imshow('Bitwise XOR', bitwise_xor)
+
+# bitwise NOT
+
+bitwise_not = cv.bitwise_not(rectangle, circle)
+cv.imshow('Bitwise NOT', bitwise_not)
+
+# bitwise Circle Not
+
+bitwise_not_circle = cv.bitwise_not(circle)
+cv.imshow('Bitwise Not Circle', bitwise_not_circle)
+
+cv.waitKey(0)
